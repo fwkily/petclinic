5d362cd59bff34f18ad7b877a92579ac7d641677
fwkily <2651960830@qq.com>
2023-05-08 15:51:26 +0800

    使用docker部署springboot3项目示例

--- src/main/resources/db/mysql/user.sql
+++ src/main/resources/db/mysql/user.sql
@@ -4,4 +4,7 @@ ALTER DATABASE petclinic
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
+
+CREATE USER 'petclinic'@'petclinic' IDENTIFIED BY 'petclinic';
+
 GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
